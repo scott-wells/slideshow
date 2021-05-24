687113988af28b962fa24581e93363eb6a8920d3
Scott Wells <efwells101@gmail.com>
2021-05-23 21:17:04 -0700

    fixed bug, finished autoslideshow with opacity fade

--- app.js
+++ app.js
@@ -3,50 +3,39 @@ const autoSlideshow = () => {
     const slideDelay = 6000
     let currentSlide = 0
 
-    slides[currentSlide].style.opacity = 1 
-
+    slides[currentSlide].classList.add('active')
+    
     const nextSlide = () => {
-        slides[currentSlide].style.opacity = 0
+        const tempCounter = currentSlide
+        slides[tempCounter].style.zIndex = 0
+        setTimeout(() => {
+            slides[tempCounter].classList.remove('active')
+        }, 2500)
         currentSlide = (currentSlide + 1) % slides.length
-        slides[currentSlide].style.opacity = 1
+        slides[currentSlide].style.zIndex = 1
+        slides[currentSlide].classList.add('active')
     }
 
     setInterval(nextSlide, slideDelay)
 }
-// const autoSlideshow = () => {
-//     const slides = document.querySelectorAll('.slides img')
-//     const slideDelay = 6000
-//     let currentSlide = 0
 
-//     slides[currentSlide].classList.add('.active')
+const changeSlide = () => {
+    const slides = document.querySelectorAll('.slides img')
+    const dots = document.querySelectorAll('.dots .dot')
 
-//     const nextSlide = () => {
-//         slides.forEach(slide => {
-//             slide.classList.remove('.active')
-//         })
-//         currentSlide = (currentSlide + 1) % slides.length
-//         slides[currentSlide].classList.add('.active')
-//     }
+    const selectSlide = (index) => {
+        slides[index].classList.toggle('active')
+        dots[index].classList.toggle('active')
+    }
 
-//     setInterval(nextSlide, slideDelay)
-// }
-
-// const changeSlide = () => {
-//     const slides = document.querySelectorAll('.slides img')
-//     const dots = document.querySelectorAll('.dots .dot')
-
-//     const selectSlide = (index) => {
-//         slides[index].classList.toggle('.active')
-//         dots[index].classList.toggle('.active')
-//     }
-
-//     dots.forEach((dot, index) => {
-//         dot.addEventListener('click', () => {
-//             selectSlide(index)
-//             currentSlide = index
-//         })
-//     })
-// }
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            console.log('CLICKED')
+            selectSlide(index)
+            currentSlide = index
+        })
+    })
+}
  
 autoSlideshow()
-// changeSlide()
+changeSlide()
