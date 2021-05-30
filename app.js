24f6f51d6ad818db1307db7b8d98c8d5a8290cb7
Scott Wells <efwells101@gmail.com>
2021-05-29 21:49:04 -0700

    Deleted files. Added manual click slide navigation.

--- app.js
+++ app.js
@@ -1,41 +1,104 @@
-const autoSlideshow = () => {
-    const slides = document.querySelectorAll('.slides img')
-    const slideDelay = 6000
-    let currentSlide = 0
+const slider = document.querySelector('.slider')
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const slides = document.querySelectorAll('.slide')
+const slideIcons = document.querySelectorAll('.slide-icon')
+const numberOfSlides = slides.length
+let slideNumber = 0
 
-    slides[currentSlide].classList.add('active')
-    
-    const nextSlide = () => {
-        const tempCounter = currentSlide
-        slides[tempCounter].style.zIndex = 0
-        setTimeout(() => {
-            slides[tempCounter].classList.remove('active')
-        }, 2500)
-        currentSlide = (currentSlide + 1) % slides.length
-        slides[currentSlide].style.zIndex = 1
-        slides[currentSlide].classList.add('active')
-    }
-
-    setInterval(nextSlide, slideDelay)
-}
-
-const changeSlide = () => {
-    const slides = document.querySelectorAll('.slides img')
-    const dots = document.querySelectorAll('.dots .dot')
-
-    const selectSlide = (index) => {
-        slides[index].classList.toggle('active')
-        dots[index].classList.toggle('active')
-    }
-
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            console.log('CLICKED')
-            selectSlide(index)
-            currentSlide = index
+// manual slide navigation
+slideIcons.forEach((icon, index) => {
+    icon.addEventListener('click', () => {
+        // remove 'active' class from every slide and slideIcon
+        slides.forEach(slide => {
+            slide.classList.remove('active')
         })
+        slideIcons.forEach(slideIcon => {
+            slideIcon.classList.remove('active')
+        })
+        // set slide and slideIcon class at current index to 'active'
+        slides[index].classList.add('active')
+        slideIcons[index].classList.add('active')
+        // set currentSlide to index
+        slideNumber = index
     })
+})
+
+// image slider next button
+// on click
+nextBtn.addEventListener('click', () => {
+    // remove 'active' class from every slide and slideIcon
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+    slideIcons.forEach(slideIcon => {
+        slideIcon.classList.remove('active')
+    })
+    // increase index
+    slideNumber ++
+    // if current index is greater than the last index, reset index to 0
+    if(slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0
+    }
+    // set slide and slideIcon class at current index to 'active'
+    slides[slideNumber].classList.add('active')
+    slideIcons[slideNumber].classList.add('active')
+})
+
+// image slider prev button
+// on click
+prevBtn.addEventListener('click', () => {
+    // remove 'active' class from every slide and slideIcon
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+    slideIcons.forEach(slideIcon => {
+        slideIcon.classList.remove('active')
+    })
+    // decrease index
+    slideNumber --
+    // if current index is less than 0, set index to last index
+    if(slideNumber < 0) {
+        slideNumber = numberOfSlides - 1
+    }
+    // set slide and slideIcon class at current index to 'active'
+    slides[slideNumber].classList.add('active')
+    slideIcons[slideNumber].classList.add('active')
+})
+
+// image slider autoplay
+// set intial state of variable 'playSlider'
+let playSlider
+// at a setInterval of 5s...
+const autoPlay = () => {
+    playSlider = setInterval(() => {
+        // remove 'active' class from every slide and slideIcon
+        slides.forEach(slide => {
+            slide.classList.remove('active')
+        })
+        slideIcons.forEach(slideIcon => {
+            slideIcon.classList.remove('active')
+        })
+        // increase index
+        slideNumber ++
+        // if current index is greater than the last index, reset index to 0
+        if(slideNumber > (numberOfSlides - 1)) {
+            slideNumber = 0
+        }
+        // set slide and slideIcon class at current index to 'active'
+        slides[slideNumber].classList.add('active')
+        slideIcons[slideNumber].classList.add('active')
+    }, 5000)
 }
- 
-autoSlideshow()
-changeSlide()
+// run the function
+autoPlay()
+
+// stop image slider autoplay on mouseover
+slider.addEventListener('mouseover', () => {
+    clearInterval(playSlider)
+})
+
+// start image slider autoplay on mouseout
+slider.addEventListener('mouseout', () => {
+    autoPlay()
+})
